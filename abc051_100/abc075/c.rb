c46665601e644d7a2ff0281a88a9010ea4e910a0
kohkb <k.oh9.380@gmail.com>
2020-01-05 18:43:26 +0900

    abc075のc

--- abc051_100/abc075/c.rb
+++ abc051_100/abc075/c.rb
@@ -1,21 +1,44 @@
-n, m = gets.chop.split.map(&:to_i)
-l = Array.new
+N, M = gets.chop.split.map(&:to_i)
+L = Array.new
 
-
-m.times do |i|
-  l << gets.chop.split.map(&:to_i)
+M.times do
+  L << gets.chop.split.map(&:to_i)
 end
 
-p l
+ans = 0
 
+M.times do |i|
+  # i番目を取り除いた配列を作る
+  l = L[0,i] + L[i+1,M-1]
 
+  # i番目を取り除いたときのグラフを作る
+  h = Hash.new{ |h, k| h[k] = [] }
 
-m.times do |i|
-  tmp = l
+  l.each do |from, to|
+    h[from] << to
+    h[to] << from
+  end
+  
+  visited = {}
+  queue = [1]
+  until queue.empty?
+    vertex = queue.shift
 
-  p tmp
-  tmp.delete_at(i)
+    next unless visited[vertex].nil?
+    visited[vertex] = true
 
+    h[vertex].each do |j|
+      queue.push(j)
+    end
+  end
 
-  # p tmp
+  # すべての頂点に訪れていなかったら橋なので+1
+  unless visited.size == N
+    ans += 1
+  end
 end
+
+p ans
+
+
+
