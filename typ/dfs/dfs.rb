052819b3ff73db08404a289ec278b9f65124ade6
kohkb <k.oh9.380@gmail.com>
2020-02-25 01:53:44 +0900

    DFS

---
+++ typ/dfs/dfs.rb
@@ -0,0 +1,22 @@
+# 深さ全探索の典型問題
+# https://atcoder.jp/contests/abc015/tasks/abc015_3
+# 重複順列でも可能
+
+@n, @k = gets.chomp.split.map(&:to_i)
+@t = @n.times.map { gets.chomp.split.map(&:to_i)}
+
+# 深さはn
+# 各dfsは必ずtrueかfalseを返す
+def dfs(val, i)
+  # n + 1番目まで来たらそのvalが0かどうかを判定する
+  return val.zero? if i == @n
+
+  # 横幅(k)分ループする
+  @k.times do |j|
+    tmp = val ^ @t[i][j]
+    return true if dfs(tmp, i + 1)
+  end
+  false
+end
+
+puts dfs(0, 0) ? 'Found' : 'Nothing'
