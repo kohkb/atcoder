c3204fda7c56cd15faa0d039214400499c9d79d6
kohkb <koh.plate@gmail.com>
2020-02-29 02:13:58 +0900

    lcmに逆元追加

--- lib/gcd.rb
+++ lib/gcd.rb
@@ -1,3 +1,5 @@
+MOD = 10**9 + 7
+
 def gcd(a, b)
   a, b = b, a if a < b
 
@@ -11,7 +13,23 @@ def lcm(a, b)
   a * b / gcd(a, b)
 end
 
+# 正しいかは不明
+def lcm_inv(a, b)
+  a * b *inv(gcd(a, b)) % MOD
+end
+
+def inv(x)
+  res = 1 # 逆元をどんどんかけるやつ
+  beki = x # 逆元を計算したい値のMOD化でのべき乗
+  (MOD - 2).to_s(2).reverse.chars do |digest|
+    res = (beki * res) % MOD if digest == '1'
+    beki = (beki * beki) % MOD
+  end
+  res
+end
+
 a, b = gets.chomp.split.map(&:to_i)
 
-puts "#{a}と#{b}の最小公倍数は#{lcm(a,b)}です"
+puts "#{a}と#{b}の最小公倍数は#{lcm_inv(a,b)}です"
+puts "#{a}と#{b}の最小公倍数は#{lcm(a,b) % MOD}です"
 puts "#{a}と#{b}の最大公約数は#{gcd(a,b)}です"
